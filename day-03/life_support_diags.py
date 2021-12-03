7223821f1e935b1fb5856d12acddeaeba22ef7f0
Chris Britton <britton@texas.net>
2021-12-03 04:49:22 -0600

    Day 3 complete

--- day-03/life_support_diags.py
+++ day-03/life_support_diags.py
@@ -1,19 +1,19 @@
 class LifeSupportDiagnostics(object):
-    LENGTH = 12
+    BITS_PER_VALUE = 12
 
     def __init__(self):
         self.values = []
 
     def o2_rating(self):
-        oxygen_values = self.values.copy()
-        return self.reduce_values(oxygen_values, self.most_common_value)
+        o2_values = self.values.copy()
+        return self.reduce_values(o2_values, self.most_common_value)
 
     def co2_rating(self):
         co2_values = self.values.copy()
         return self.reduce_values(co2_values, self.least_common_value)
 
     def reduce_values(self, values, bit_criteria_function):
-        for position in range(LifeSupportDiagnostics.LENGTH):
+        for position in range(LifeSupportDiagnostics.BITS_PER_VALUE):
             self.discard_values(values, position, bit_criteria_function)
             if len(values) == 1:
                 return values[0]
@@ -22,22 +22,19 @@ class LifeSupportDiagnostics(object):
         index = 0
         bit_value = bit_criteria_function(values, position)
         while index < len(values):
-            if self.get_bit(values[index], position) != bit_value:
+            if self.get_bit_value(values[index], position) != bit_value:
                 del values[index]
             else:
                 index += 1
 
-    def get_bit(self, value, position):
-        return (value >> LifeSupportDiagnostics.LENGTH - position - 1) & 1
+    def get_bit_value(self, value, position):
+        return (value >> LifeSupportDiagnostics.BITS_PER_VALUE - position - 1) & 1
 
     def position_counts(self, values, position):
-        zero_count = one_count = 0
+        counts = [0] * 2
         for value in values:
-            if self.get_bit(value, position):
-                one_count += 1
-            else:
-                zero_count += 1
-        return [zero_count, one_count]
+            counts[self.get_bit_value(value, position)] += 1
+        return counts
 
     def most_common_value(self, values, position):
         [zero_count, one_count] = self.position_counts(values, position)
@@ -56,8 +53,7 @@ def main():
     with open('diagnostics.data', 'r') as data_file:
         line = data_file.readline()
         while line:
-            value = line.strip()
-            life_support_diagnostics.values.append(int(value, 2))
+            life_support_diagnostics.values.append(int(line.strip(), 2))
             line = data_file.readline()
     print("value: {}".format(life_support_diagnostics.rating()))
 
