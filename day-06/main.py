c2208ddbff6b8dd247af67aecef130d5c3ef5480
Chris Britton <britton@texas.net>
2021-12-06 02:55:55 -0600

    Day 6 part 2 before refactor to buckets

--- day-06/main.py
+++ day-06/main.py
@@ -1,71 +1,48 @@
-
-class LanternFish(object):
-    SPAWN_CYCLE_DAYS = 7
-    NEWBORN_SPAWN_DELAY_DAYS = 2
-
-    def __init__(self, age_days):
-        self.age_days = age_days
-
-    def add_data(self, input_line):
-        pass
-
-    def to_string(self):
-        return '{}'.format(self.age_days)
+import numpy
+from time import process_time
 
 
 class Simulator(object):
+    MAX_SIZE = 300000
     MAX_DAYS = 256
-    PART_1_DAYS = 80
-    PART_2_DAYS = 256
+    SPAWN_CYCLE_DAYS = 7
+    NEWBORN_SPAWN_DELAY_DAYS = 2
+    NEWBORN_CYCLE_DAYS = SPAWN_CYCLE_DAYS + NEWBORN_SPAWN_DELAY_DAYS
 
-    def __init__(self):
-        self.all_lantern_fish = []
-        self.newborn_lantern_fish = []
+    def __init__(self, input_line):
+        self.total_lantern_fish = 0
+        init_ages_days = input_line.strip().split(',')
+        self.all_lantern_fish = numpy.full(Simulator.MAX_SIZE, -2, dtype=numpy.int8)
+        for i in range(len(init_ages_days)):
+            self.all_lantern_fish[i] = int(init_ages_days[i])
+            self.total_lantern_fish += 1
         self.day = 0
-
-    def create_lantern_fish(self, age_days):
-        self.all_lantern_fish.append(LanternFish(age_days))
-
-    def create_newborn_lantern_fish(self):
-        self.newborn_lantern_fish.append(
-            LanternFish(LanternFish.SPAWN_CYCLE_DAYS + LanternFish.NEWBORN_SPAWN_DELAY_DAYS - 1))
 
     def run(self):
         for day in range(Simulator.MAX_DAYS):
+            start_time = process_time()
             self.tick()
-            if day == Simulator.PART_1_DAYS - 1:
-                self.print_result()
-            if day == Simulator.PART_2_DAYS - 1:
-                self.print_result()
+            end_time = process_time()
+            print('Total after {} days: {}'.format(self.day, self.total_lantern_fish))
+            print('    elapsed time: {} seconds'.format(end_time - start_time))
 
     def tick(self):
         self.day += 1
-        for lantern_fish in self.all_lantern_fish:
-            lantern_fish.age_days -= 1
-            if lantern_fish.age_days < 0:
-                lantern_fish.age_days = LanternFish.SPAWN_CYCLE_DAYS - 1
-                self.create_newborn_lantern_fish()
-        self.all_lantern_fish.extend(self.newborn_lantern_fish)
-        self.newborn_lantern_fish.clear()
+        self.all_lantern_fish[self.all_lantern_fish >= 0] -= 1
+        newborn_count = (self.all_lantern_fish == -1).sum()
+        self.all_lantern_fish[self.all_lantern_fish == -1] = Simulator.SPAWN_CYCLE_DAYS - 1
+        if newborn_count:
+            self.all_lantern_fish[self.total_lantern_fish:self.total_lantern_fish + newborn_count] = Simulator.NEWBORN_CYCLE_DAYS - 1
+            self.total_lantern_fish += newborn_count
 
     def print_result(self):
         print('Total after {} days: {}'.format(self.day, len(self.all_lantern_fish)))
 
-    def to_string(self):
-        result = ''
-        for lantern_fish in self.all_lantern_fish:
-            result += lantern_fish.to_string()
-            result += '\n'
-        return result
-
 
 def run():
-    simulator = Simulator()
     with open('data.txt', 'r') as data_file:
         input_line = data_file.readline()
-        init_ages_days = input_line.strip().split(',')
-        for age in init_ages_days:
-            simulator.create_lantern_fish(int(age))
+        simulator = Simulator(input_line)
         simulator.run()
 
 
