5d82babc5d3e9b652cba851d252cee964eb420a4
Chris Britton <britton@texas.net>
2021-12-06 06:09:46 -0600

    Day 6 cleanup

--- day-06/main.py
+++ day-06/main.py
@@ -1,5 +1,4 @@
 import numpy
-from scipy.ndimage.interpolation import shift
 
 
 class Simulator(object):
@@ -24,44 +23,30 @@ class Simulator(object):
 
     def tick(self):
         self.day += 1
-
         self.add_adolescents_to_adults()
         self.shift_young_generations()
         self.spawn()
 
     def spawn(self):
         spawn_index = (self.day - 1) % Simulator.SPAWN_CYCLE_DAYS
-        print('spawning {} from generations {}'.format(self.generations[spawn_index], spawn_index))
         self.generations[Simulator.NEWBORN_CYCLE_DAYS] = self.generations[spawn_index]
 
     def add_adolescents_to_adults(self):
-        # new_adult_index = (self.day - Simulator.NEWBORN_SPAWN_DELAY_DAYS - 1) % Simulator.SPAWN_CYCLE_DAYS
-        new_adult_index = (self.day - 2) % Simulator.SPAWN_CYCLE_DAYS
-        print('=============')
-        print('day: {}, new_adult_index: {}, generations: {}'.format(self.day, new_adult_index, self.generations))
-        print('adding {} spawned on day {} to {}'.format(self.generations[Simulator.NEWBORN_CYCLE_DAYS - 2],
-                                                         self.day - Simulator.NEWBORN_SPAWN_DELAY_DAYS, new_adult_index))
-        self.generations[new_adult_index] += self.generations[Simulator.NEWBORN_CYCLE_DAYS - 2]
+        new_adult_index = (self.day - Simulator.NEWBORN_SPAWN_DELAY_DAYS) % Simulator.SPAWN_CYCLE_DAYS
+        self.generations[new_adult_index] += self.generations[
+            Simulator.NEWBORN_CYCLE_DAYS - Simulator.NEWBORN_SPAWN_DELAY_DAYS]
 
     def shift_young_generations(self):
-        print('before shift: {}'.format(self.generations))
         for i in range(Simulator.SPAWN_CYCLE_DAYS, Simulator.AGE_BUCKET_SIZE - 1):
             self.generations[i] = self.generations[i + 1]
         self.generations[Simulator.AGE_BUCKET_SIZE - 1] = 0
-        # shift(self.generations[Simulator.SPAWN_CYCLE_DAYS:], -1, cval=0)
-        print('after shift:  {}'.format(self.generations))
 
 
-def run():
-    numpy.set_printoptions(linewidth=1000)
+def main():
     with open('data.txt', 'r') as data_file:
         input_line = data_file.readline()
         simulator = Simulator(input_line)
         simulator.run()
-
-
-def main():
-    run()
 
 
 main()
