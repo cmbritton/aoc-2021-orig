9e618e314b4470a98fc86866218f18096ebab56d
Chris Britton <britton@texas.net>
2021-12-10 03:22:14 -0600

    Day 10 refactor after learning new things
    by looking at this example from someone who appears to be a much more skilled python programmer than me.
    https://github.com/benediktwerner/AdventOfCode/blob/master/2021/day10/sol.py

--- day-10/main.py
+++ day-10/main.py
@@ -1,7 +1,14 @@
-CLOSE_TOKENS = [')', '}', ']', '>']
+# Although my original revision of this worked correctly, I'm still trying to learn
+# python features. This revision was after I looked at
+# https://github.com/benediktwerner/AdventOfCode/blob/master/2021/day10/sol.py
+#
+# Things I learned:
+#   - "for" loop has an "else" clause that is executed when the loop encounters no break or continue.
+#   - // integer division
+#   - If a constant is used in just one place, sometimes it's more readable to ditch the constant and use its value in
+#     that place.
+
 TOKEN_MATCH = {')': '(', '}': '{', ']': '[', '>': '<', '(': ')', '{': '}', '[': ']', '<': '>'}
-UNEXPECTED_TOKEN_COST = {')': 3, ']': 57, '}': 1197, '>': 25137}
-INCOMPLETE_TOKEN_COST = {')': 1, ']': 2, '}': 3, '>': 4}
 
 
 with open('data.txt', 'r') as data_file:
@@ -15,39 +22,36 @@ for line in lines:
 bad_tokens = []
 completion_sequences = []
 for line_idx in range(len(tokens_by_line)):
-    skip_remaining = False
     tokens = []
     for col_idx in range(len(tokens_by_line[line_idx])):
         token = tokens_by_line[line_idx][col_idx]
-        if token in CLOSE_TOKENS:
+        if token in [')', '}', ']', '>']:
             if token != TOKEN_MATCH[tokens[-1]]:
                 bad_tokens.append(token)
-                skip_remaining = True
                 break
             else:
                 tokens.pop()
         else:
             tokens.append(token)
-    if skip_remaining:
-        continue
-    if len(tokens) != 0:
-        completion_sequence = []
-        for token in reversed(tokens):
-            completion_sequence.append(TOKEN_MATCH[token])
-        completion_sequences.append(completion_sequence)
-        continue
+    else:
+        if len(tokens) != 0:
+            completion_sequence = []
+            for token in reversed(tokens):
+                completion_sequence.append(TOKEN_MATCH[token])
+            completion_sequences.append(completion_sequence)
+            continue
 
 score = 0
 for token in bad_tokens:
-    score += UNEXPECTED_TOKEN_COST[token]
+    score += {')': 3, ']': 57, '}': 1197, '>': 25137}[token]
 print('Part 1, corrupted lines score={}'.format(score))
 
 incomplete_line_scores = []
 for completion_sequence in completion_sequences:
     score = 0
     for token in completion_sequence:
-        score = (score * 5) + INCOMPLETE_TOKEN_COST[token]
+        score = (score * 5) + {')': 1, ']': 2, '}': 3, '>': 4}[token]
     incomplete_line_scores.append(score)
 incomplete_line_scores.sort()
-middle_score = incomplete_line_scores[int((len(incomplete_line_scores) - 1) / 2)]
+middle_score = incomplete_line_scores[(len(incomplete_line_scores) - 1) // 2]
 print('Part 2, middle incomplete line score={}'.format(middle_score))
