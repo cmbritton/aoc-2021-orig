2a928d464fd80bbe80d7172fc41975cb4daf8513
Chris Britton <britton@texas.net>
2021-12-13 23:43:52 -0600

    Day 13 complete

--- day-13/main.py
+++ day-13/main.py
@@ -1,17 +1,26 @@
 import numpy as np
 
 
-def fold_sheet_up(sheet, fold_y) -> np.ndarray:
-    shape = max(fold_y, sheet.shape[0] - fold_y - 1), sheet.shape[1]
+def fold_sheet_up(unfolded_sheet, fold_y) -> np.ndarray:
+    unfolded_height, unfolded_width = unfolded_sheet.shape
+    upper_height = fold_y
+    lower_height = unfolded_sheet.shape[0] - fold_y - 1
+    width = unfolded_sheet.shape[1]
+    folded_height = max(upper_height, lower_height)
+    shape = max(upper_height, lower_height), width
     upper_portion = np.zeros(shape, dtype=bool)
-    lower_portion = np.zeros(shape, dtype=bool)
-    if fold_y >= sheet.shape[0] - fold_y - 1:
-        upper_portion[0:fold_y - 1, 0:shape[1]] = sheet[0:fold_y - 1, 0:shape[1]]
-        lower_portion[fold_y - (sheet.shape[0] - fold_y - 1):shape[0], 0:shape[1]] = sheet[-1:fold_y:-1, 0:shape[1]]
-    else:
-        upper_portion[shape[0] - fold_y:shape[0], 0:shape[1]] = sheet[0:fold_y, 0:shape[1]]
-        lower_portion[0:shape[0] - fold_y - 1, 0:shape[1]] = sheet[-1:fold_y:-1, 0:shape[1]]
-    return np.bitwise_or(upper_portion, lower_portion)
+    folded_lower_portion = np.zeros(shape, dtype=bool)
+
+    upper_start_y = folded_height - fold_y
+    upper_end_y = lower_end_y = folded_height
+    lower_start_y = 0
+    if fold_y > unfolded_height // 2:
+        lower_start_y = folded_height - (lower_height - 1)
+        if not unfolded_height % 2:
+            lower_start_y -= 1
+    upper_portion[upper_start_y:upper_end_y, 0:width] = unfolded_sheet[0:fold_y, 0:width]
+    folded_lower_portion[lower_start_y:lower_end_y, 0:width] = unfolded_sheet[-1:fold_y:-1, 0:width]
+    return np.bitwise_or(upper_portion, folded_lower_portion)
 
 
 def fold_sheet_left(sheet, fold_x) -> np.ndarray:
