c6afb11fd28892f5e96fdc35536204a577513480
Chris Britton <britton@texas.net>
2021-12-15 03:28:42 -0600

    Day 14 complete

--- day-14/main.py
+++ day-14/main.py
@@ -1,11 +1,6 @@
+from collections import defaultdict
+
 MAX_STEPS = 40
-
-
-class Node(object):
-
-    def __init__(self, value, next_node=None):
-        self.value = value
-        self.next_node = next_node
 
 
 def read_input() -> (str, list):
@@ -22,73 +17,57 @@ def read_input() -> (str, list):
     return template, rules
 
 
-def print_polymer(head):
-    current = head
-    print(current.value, end='')
-    while current.next_node:
-        current = current.next_node
-        print(current.value, end='')
-    print('')
-
-
-def apply_rule(previous, current, rule):
-    if previous.value == rule[0][0] and current.value == rule[0][1]:
-        previous.next_node = Node(rule[1], current)
-
-
-def apply_rules(head, rules):
-    current = head
-    while current.next_node:
-        previous = current
-        current = current.next_node
-        for rule in rules:
-            apply_rule(previous, current, rule)
-
-
-def count_elements(head) -> {}:
-    element_counts = {}
-    current = head
-    while current:
-        if current.value in element_counts:
-            element_counts[current.value] += 1
-        else:
-            element_counts[current.value] = 1
-        current = current.next_node
-    return element_counts
-
-
-def get_solution(head) -> int:
-    element_counts = count_elements(head)
-    min_element = None
-    max_element = None
-    for element, count in element_counts.items():
-        if not min_element:
-            min_element = max_element = element
-            continue
-        if count < element_counts[min_element]:
-            min_element = element
-        if count > element_counts[max_element]:
-            max_element = element
-    return element_counts[max_element] - element_counts[min_element]
+def get_solution(element_count_map) -> int:
+    most_common_element = None
+    least_common_element = None
+    for element, count in element_count_map.items():
+        if not most_common_element or count > element_count_map[most_common_element]:
+            most_common_element = element
+        if not least_common_element or (count < element_count_map[least_common_element] != 0):
+            least_common_element = element
+    return element_count_map[most_common_element] - element_count_map[least_common_element]
 
 
 def main() -> None:
-    head = None
-    current = None
     template, rules = read_input()
-    for element in list(template):
-        if not head:
-            head = Node(element)
-            current = head
-            continue
-        previous = current
-        current = Node(element)
-        previous.next_node = current
+    element_count_map = defaultdict(lambda: 0)
+    pair_count_map = defaultdict(lambda: 0)
+    added_pair_count_map = defaultdict(lambda: 0)
+    added_elements_count_map = defaultdict(lambda: 0)
+    removed_pair_counts = set()
+
+    for i in range(len(template)):
+        element_count_map[template[i]] += 1
+
+    for i in range(len(template) - 1):
+        pair_count_map[template[i] + template[i + 1]] += 1
+
     for step in range(MAX_STEPS):
-        apply_rules(head, rules)
+        added_pair_count_map.clear()
+        added_elements_count_map.clear()
+        removed_pair_counts.clear()
+        for rule in rules:
+            pre_rule_pair_count = pair_count_map[rule[0]]
+            added_elements_count_map[rule[1]] += pre_rule_pair_count
+            added_pair_count_map[rule[0][0] + rule[1]] += pre_rule_pair_count
+            added_pair_count_map[rule[1] + rule[0][1]] += pre_rule_pair_count
+            removed_pair_counts.add(rule[0])
+            # if pre_rule_pair_count != 0:
+            #     print('rule {} matched {} pairs'.format(rule, pre_rule_pair_count))
+        for pair in removed_pair_counts:
+            # if count > 0:
+            #     print('added {}, {}'.format(pair, count))
+            del pair_count_map[pair]
+        for pair, count in added_pair_count_map.items():
+            # if count > 0:
+            #     print('added {}, {}'.format(pair, count))
+            pair_count_map[pair] += count
+        for element, count in added_elements_count_map.items():
+            element_count_map[element] += count
         if step == 9:
-            print('Part 1, most common - least common after 10 steps = {}'.format(get_solution(head)))
-    print('Part 2, most common - least common after 40 steps = {}'.format(get_solution(head)))
+            print('Part 1, most common - least common after {} steps = {}'.format(step + 1, get_solution(
+                element_count_map)))
+    print('Part 2, most common - least common after {} steps = {}'.format(MAX_STEPS, get_solution(element_count_map)))
 
 
 main()
