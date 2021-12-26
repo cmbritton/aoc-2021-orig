7ad5ad52d773b8ec4d92b6d197413cbdf3ab86f3
Chris Britton <britton@texas.net>
2021-12-26 17:08:44 -0600

    Day 17 complete

--- day-17/main.py
+++ day-17/main.py
@@ -3,46 +3,93 @@ from typing import Tuple
 import time
 
 
-# def reverse_step(x, y) -> Tuple[int, int]:
-
-def plot_reverse_trajectory(trench_x, trench_y, min_x, max_x, min_y, max_y) -> list:
-    trajectory = []
-    x = trench_x
-    y = trench_y
-    while x > 0:
-        x, y = reverse_step(x, y)
-        if x == 0 and y == 0:
-            return trajectory.reverse()
-        if x < 0:
-            return None
-        trajectory.append((x, y))
+def is_target_boundary_exceeded(point, target_dim):
+    return point[0] > target_dim[1] or point[1] < target_dim[2]
 
 
-def possible_x_values(x):
-    x_values = [x]
-    reverse_delta = 1
-    while x >= 0:
-        if x == 0:
-            x_values.reverse()
-            return x_values
-        else:
-            x -= reverse_delta
-            x_values.append(x)
-            reverse_delta += 1
-    return []
+def is_in_target_area(point, target_dim):
+    return (target_dim[0] <= point[0] <= target_dim[1]) and (target_dim[2] <= point[1] <= target_dim[3])
 
 
-def do_run(min_x, max_x, min_y, max_y) -> Tuple[int, int]:
-    for trench_x in range(min_x, max_x + 1):
-        for trench_y in range(min_y, max_y + 1):
-            pass
-            # start_x, start_y = plot_reverse_trajectory(trench_x, trench_y, min_x, max_x, min_y, max_y)
-        x_values = possible_x_values(trench_x)
-        print(f'x_values = {str(x_values)}')
-    return 1, 1
+def step(x, y, delta_x, delta_y):
+    x += delta_x
+    if delta_x:
+        delta_x -= 1
+    y += delta_y
+    delta_y -= 1
+    return x, y, delta_x, delta_y
 
 
-def init_data() -> Tuple[int, int, int, int]:
+def simulate(start_velocity, target_dim):
+    path = []
+    current_x = 0
+    delta_x = start_velocity[0]
+    current_y = 0
+    delta_y = start_velocity[1]
+    while not is_target_boundary_exceeded((current_x, current_y), target_dim):
+        current_x, current_y, delta_x, delta_y = step(current_x, current_y, delta_x, delta_y)
+        path.append((current_x, current_y))
+        if is_in_target_area((current_x, current_y), target_dim):
+            return path
+    return None
+
+
+def highest_y_path(valid_paths):
+    best_start_velocity = None
+    best_path = None
+    max_y = -999999
+    for start_velocity, path in valid_paths.items():
+        for value in path:
+            if value[1] > max_y:
+                max_y = value[1]
+                best_start_velocity = start_velocity
+                best_path = path
+    return best_start_velocity, best_path, max_y
+
+
+def end_x(start_x):
+    value = 0
+    while start_x > 0:
+        value += start_x
+        start_x -= 1
+    return value
+
+
+def get_min_start_x(target_dim):
+    start_x = 0
+    while end_x(start_x) < target_dim[0]:
+        start_x += 1
+    return start_x
+
+
+def do_run(target_dim):
+    valid_paths = {}
+    for x in range(get_min_start_x(target_dim), target_dim[1] + 1):
+        for y in range(500, target_dim[2] - 1, -1):
+            path = simulate((x, y), target_dim)
+            if path:
+                valid_paths[(x, y)] = path
+
+    return valid_paths
+
+
+def elapsed_time(start_time: int, end_time: int) -> str:
+    t = end_time - start_time
+    unit = 'seconds'
+    if t < 1:
+        t = t * 1000
+        unit = 'milliseconds'
+    if t < 1:
+        t = t * 1000
+        unit = 'microseconds'
+    if t < 1:
+        t = t * 1000
+        unit = 'nanoseconds'
+
+    return f'{t:.2f} {unit}'
+
+
+def init_data():
     with open('data.txt', 'r') as data_file:
         line = data_file.readline()
     m = re.search(r'target area: x=([-0-9]*)\.\.([-0-9]*), y=([-0-9]*)\.\.([-0-9]*)', line)
@@ -50,14 +97,15 @@ def init_data() -> Tuple[int, int, int, int]:
 
 
 def main() -> None:
-    min_x, max_x, min_y, max_y = init_data()
+    target_dim = init_data()
 
     start_time = time.perf_counter()
-    x, y = do_run(min_x, max_x, min_y, max_y)
+    valid_paths = do_run(target_dim)
+    best_start_velocity, best_path, max_y = highest_y_path(valid_paths)
     end_time = time.perf_counter()
-    print(f'Part 1, init velocity = {(x, y)}')
-    # print(f'Part 2, value = {value}')
-    # print(f'Elapsed time: {elapsed_time(start_time, end_time)}')
+    print(f'\nPart 1, highest y: {max_y}')
+    print(f'Part 2, Total valid paths = {len(valid_paths)}')
+    print(f'Elapsed time: {elapsed_time(start_time, end_time)}')
 
 
 if __name__ == '__main__':
